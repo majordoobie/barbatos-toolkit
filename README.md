b3372396db53c900f34e9a6b6700fe5197ac772f
Jason Jason <Sgtmajordoobie@live.com>
2019-10-18 22:37:42 -0400

    ignore

--- README.md
+++
@@ -1,3 +0,0 @@
-# dragontoolkit
-
-## Collection of random tools for CTFs or just scripts that don't have a project to go into
--- random_hackytools/public_ip.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-from requests import get
-ip = get('https://api.ipify.org').text
-print('My exit node is: {}'.format(ip))
