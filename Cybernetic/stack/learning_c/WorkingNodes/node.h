6eb7b32dee5822eb9e1894064fac4c4d257a82fc
majordoobie <Sgtmajordoobie@live.com>
2021-03-24 15:27:59 -0400

    vault backup: 20210324-15:27:58

--- Cybernetic/stack/learning_c/WorkingNodes/node.h
+++ Cybernetic/stack/learning_c/WorkingNodes/node.h
@@ -6,6 +6,16 @@ typedef struct
 
 } Node;
 
+struct Nod2
+{
+    int value;
+    struct Node_var *left;
+    struct Node_var *right;
+
+};
+
+typedef Nod2 Nodet;
+
 Node *make_node(int value, Node *head_node);
 void to_string(Node *node);
 void iter_nodes(Node *head_node);
